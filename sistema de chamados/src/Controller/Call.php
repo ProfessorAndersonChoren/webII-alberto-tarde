2fcad85c8a2d59362812a5a8c5ce7b0f53c05a85
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-22 16:41:18 -0300

    Sistema de chamados - Inclusão de chamados

--- sistema de chamados/src/Controller/Call.php
+++ sistema de chamados/src/Controller/Call.php
@@ -2,10 +2,11 @@
 
 namespace QI\SistemaDeChamados\Controller;
 
+use Exception;
 use QI\SistemaDeChamados\Model\Equipment;
+use QI\SistemaDeChamados\Model\Repository\CallRepository;
 use QI\SistemaDeChamados\Model\User;
 use QI\SistemaDeChamados\Model\Call;
-use QI\SistemaDeChamados\Model\Repository\Connection;
 
 require_once dirname(dirname(__DIR__)) . "/vendor/autoload.php";
 
@@ -24,14 +25,18 @@ function insert()
 {
     if (empty($_POST)) {
         $_SESSION["msg_error"] = "Ops, houve um erro inesperado!!!";
-        header("../View/message.php");
+        header("location:../View/message.php");
         exit;
     }
     $equipment = new Equipment(
         intval($_POST["floor"]),
         $_POST["room"]
     );
+    $equipment->id = $_POST["pc_number"];
+
     $user = new User($_POST["user_email"]);
+    $user->id = 1;
+
     $call = new Call(
         $user,
         $equipment,
@@ -41,9 +46,24 @@ function insert()
     // TODO Validar os dados recebidos
     $errors = array();
 
-    // TODO Realizar a persistência
-    if(empty($errors)){
-        $conn = Connection::getConnection();
-        var_dump($conn);
+    if (!empty($errors)) {
+    }
+
+    try {
+        $call_repository = new CallRepository();
+        $result = $call_repository->insert($call);
+
+        echo "Passou";
+
+        if ($result) {
+            $_SESSION["msg_success"] = "Chamado registrado com sucesso!!!";
+        } else {
+            $_SESSION["msg_warning"] = "Lamento, não foi possível registrar o chamado!!!";
+        }
+    } catch (Exception $e) {
+        $_SESSION["msg_error"] = "Ops, houve um erro inesperado em nossa base de dados";
+    } finally {
+        header("location:../View/message.php");
+        exit;
     }
 }
