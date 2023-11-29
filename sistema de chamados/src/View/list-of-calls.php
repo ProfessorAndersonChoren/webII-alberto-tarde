73bb3418d8cc793e2b4345c929dbb3a653383c2b
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-29 16:49:55 -0300

    Sistema de chamados - Listagem de chamados

--- sistema de chamados/src/View/list-of-calls.php
+++ sistema de chamados/src/View/list-of-calls.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP HelpDesk - Lista de chamados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 
 <body class="m-5">
@@ -17,6 +18,61 @@
         </div>
         <a href="../Controller/Auth.php?operation=logout" class="text-white text-decoration-none">Sair</a>
     </nav>
+    <main class="m-5">
+        <table class="table table-bordered table-primary">
+            <thead>
+                <th>#</th>
+                <th>Nome do usuário</th>
+                <th>Número do equipamento</th>
+                <th>Classificação</th>
+                <th>Descrição</th>
+                <th>Observações</th>
+                <th>Ações</th>
+            </thead>
+            <tbody>
+                <?php
+                if (empty($_SESSION["list_of_calls"])) :
+                ?>
+                    <td colspan=6 class="text-center fw-bold">
+                        Não existem chamados cadastrados
+                    </td>
+                <?php
+                endif;
+                foreach ($_SESSION["list_of_calls"] as $call) :
+                ?>
+                    <tr>
+                        <td>
+                            <?= $call["id"] ?>
+                        </td>
+                        <td>
+                            <?= $call["name"] ?>
+                        </td>
+                        <td>
+                            <?= $call["equipment_id"] ?>
+                        </td>
+                        <td>
+                            <?= $call["classification"] ?>
+                        </td>
+                        <td>
+                            <?= $call["description"] ?>
+                        </td>
+                        <td>
+                            <?= (empty($call["notes"])) ? "Não existem observações" : $call["notes"] ?>
+                        </td>
+                        <td>
+                            <div class="btn-group">
+                                <a href="../Controller/Call.php?operation=findOne&code=<?= $call["id"] ?>" class="btn btn-warning" title="Editar chamado">
+                                    <i class="bi bi-pencil"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php
+                endforeach;
+                ?>
+            </tbody>
+        </table>
+    </main>
 </body>
 
 </html>
