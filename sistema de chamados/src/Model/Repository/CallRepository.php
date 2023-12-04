fbafd82c80e6c30e988a57b357ec7df3ad6e6b54
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-12-04 16:54:40 -0300

    Sistema de chamados - Busca de chamados

--- sistema de chamados/src/Model/Repository/CallRepository.php
+++ sistema de chamados/src/Model/Repository/CallRepository.php
@@ -22,22 +22,28 @@ class CallRepository
     public function insert($call)
     {
         $stmt = $this->connection->prepare("insert into calls values (null,?,?,?,?,?);");
-        $stmt->bindParam(1,$call->user->id);
-        $stmt->bindParam(2,$call->equipment->id);
-        $stmt->bindParam(3,$call->classification);
-        $stmt->bindParam(4,$call->description);
-        $stmt->bindParam(5,$call->notes);
+        $stmt->bindParam(1, $call->user->id);
+        $stmt->bindParam(2, $call->equipment->id);
+        $stmt->bindParam(3, $call->classification);
+        $stmt->bindParam(4, $call->description);
+        $stmt->bindParam(5, $call->notes);
         return $stmt->execute();
     }
 
-    public function findAll(){
+    public function findAll()
+    {
         $stmt = $this->connection->query("select c.*,u.name from calls c inner join users u on c.user_id = u.id;");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function findOne($id){}
+    public function findOne($id)
+    {
+        $stmt = $this->connection->query("select c.*,u.name,u.email,e.floor,e.room from calls c inner join users u on c.user_id = u.id inner join equipments e on c.equipment_id = e.id;");
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
-    public function delete($id){
+    public function delete($id)
+    {
         $stmt = $this->connection->query("delete from calls where id=$id");
         return $stmt->execute();
     }
